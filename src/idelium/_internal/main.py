8dc7fb30b891edae393228985b8c70f2ab37806c
Idel Fuschini <idel.fuschini@double77.com>
2023-08-03 17:50:06 +0200

    Update main.py

--- src/idelium/_internal/main.py
+++ src/idelium/_internal/main.py
@@ -20,7 +20,7 @@ idelium=StartManager()
 printer=InitPrinter()
 ideliumws=IdeliumWs()
 idelium_cl_lib=InitIdelium()
-IDELIUM_VERSION='1.0.3'
+IDELIUM_VERSION='1.0.5'
 
 def main(args: Optional[List[str]] = None) -> int:
     printer.print_important_text("Idelium Command Line " + IDELIUM_VERSION)
