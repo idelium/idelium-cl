ac963f076dba87abe70a1750b1926d7a27b12613
Idel Fuschini <idel.fuschini@double77.com>
2023-10-10 11:01:22 +0200

    Update ideliumpostman.py
    Supporto Auth authentication, except auth1

--- src/idelium/_internal/thirdparties/ideliumpostman.py
+++ src/idelium/_internal/thirdparties/ideliumpostman.py
@@ -1,26 +1,40 @@
 from __future__ import absolute_import
-from idelium._internal.commons.postmantranslate import PostmanTranslate
-from datetime import datetime
-from requests_oauthlib import OAuth1
-from argparse import HelpFormatter
 import requests
 import json
 import sys
+import base64
+import time
+import calendar
+from requests_hawk import HawkAuth
+from hashlib import sha256
+from idelium._internal.commons.postmantranslate import PostmanTranslate
+from datetime import datetime
+from requests_oauthlib import OAuth2
+from argparse import HelpFormatter
+import urllib.parse
+
 from re import A
 from urllib.parse import urlencode
 from idelium._internal.commons.ideliumprinter import InitPrinter
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from idelium._internal.commons.outhsignature import OuthSignature
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
 
 
 printer = InitPrinter()
 
 
 class PostmanCollection:
-        
+    
+    def find_element_in_array(self, array, key, value):
+        for i in array:
+            if i[key]==value:
+                return i
+        return None        
     def get_payload(self,request):
         return_data={}
-        method=''
+        method='' 
         if 'method' in request:
             method=request['method']
             if method == 'raw':
@@ -34,6 +48,14 @@ class PostmanCollection:
             method = request['mode']
             if method=='raw':
                 return_data = request['raw']
+            elif method == 'formdata':
+                return_data = self.get_parser(request['formdata'])
+            elif method == 'urlencoded':
+                return_data = self.get_parser(request['urlencoded'])
+            else:
+                print("mode not found: " + method)
+                sys.exit()
+
         else:
                 print("method not found: " + method)
                 sys.exit()
@@ -66,25 +88,87 @@ class PostmanCollection:
 
     def get_auth(self, auth):
         type_auth=auth['type']
-        headers=None
+        authReturn = None
         if type_auth == 'oauth1':
-            headers={
-                'Authorization': self.get_parser(auth['oauth1'], 'oauth1')
+            consumer_key = self.find_element_in_array(
+                auth['oauth1'], 'key', 'consumerKey')['value']
+            consumer_secret = self.find_element_in_array(
+                auth['oauth1'], 'key', 'consumerSecret')['value']
+            signature_method = self.find_element_in_array(
+                auth['oauth1'], 'key', 'signatureMethod')['value']
+            nonce = self.find_element_in_array(
+                auth['oauth1'], 'key', 'nonce')['value']
+            token = self.find_element_in_array(
+                auth['oauth1'], 'key', 'token')['value']
+            version = self.find_element_in_array(
+                auth['oauth1'], 'key', 'version')['value']
+            timestamp = str(self.find_element_in_array(
+                auth['oauth1'], 'key', 'timestamp')['value'])
+            
+            signature_base_string=OuthSignature.create_parameter_string(consumer_key,nonce,signature_method,timestamp,token,version)
+            print (signature_base_string)
+            signature=urllib.parse.quote(OuthSignature.create_signature(consumer_secret + '&',signature_base_string,signature_method))
+            content = {
+                'Authorization' : 'OAuth oauth_consumer_key="' + consumer_key + '",oauth_signature_method="' + signature_method + '",oauth_timestamp="'+ timestamp + '",oauth_nonce="' + nonce +'",oauth_token="' + token + '",oauth_version="' + version +'",oauth_signature="'+signature + '"'  
             }
-        return headers
+            authReturn = {
+                'type': 'headers',
+                'content': content
+            }
+        elif type_auth == 'basic':
+            username = self.find_element_in_array(
+                auth['basic'], 'key', 'username')['value']
+            password = self.find_element_in_array(auth['basic'],'key','password')['value']
+            string_authentication=username + ':' + password
+            message_bytes =string_authentication.encode('ascii')
+            base64_bytes = base64.b64encode(message_bytes)
+            base64_message = base64_bytes.decode('ascii')
+            authReturn = {
+                'type': 'headers',
+                'content': {
+                    'Authorization': 'Basic ' + base64_message
+                }
+            }
+        elif type_auth == 'digest':
+            authReturn = {
+                'type': 'getheaders',
+                'content': None
+            }
+        elif type_auth == 'hawk':
+            authid=self.find_element_in_array(
+                auth['hawk'], 'key', 'authId')['value']
+            authkey = self.find_element_in_array(
+                auth['hawk'], 'key', 'authKey')['value']
+            hawk_auth = HawkAuth(id=authid, key=authkey)
+            authReturn = {
+                'type': 'hawk',
+                'content': hawk_auth
+            }
+        return authReturn
             
 
-    def connection_test(self,request_test):
+    def connection_test(self,request_test,name_test,debug):
         ''' start '''
         method=request_test['method']
         url=request_test['url']['raw']
         headers = self.get_parser(request_test['header'])
-        start_time=datetime.now()
+        start_time = datetime.now()
+        payload = {}
+        auth=None
+        redirect_allow=False
+        auth_headers=None
         if 'auth' in request_test:
-            headers = self.get_auth(request_test['auth'])
+            auth_headers=self.get_auth(request_test['auth'])
+            if auth_headers['type'] == 'headers':
+                headers = auth_headers['content']
+            elif auth_headers['type'] == 'hawk':
+                auth = auth_headers['content']
+            elif auth_headers['type'] == 'outh1':
+                auth = auth_headers['content']
+                redirect_allow = True
+
         if method == "POST" or method == "PUT" or method == "PATCH" or method == "DELETE":
             files={}
-            payload = ''
             body = self.get_payload(request_test['body'])
         
             if body['method']=='formdata':
@@ -94,43 +178,61 @@ class PostmanCollection:
             if method== "POST":
                 req = requests.post(url,
                                     headers=headers,
+                                    auth=auth,
                                     data=json.dumps(payload),
                                     files=files,
+                                    allow_redirects=redirect_allow,
                                     verify=False)
+                
             elif method == "PUT":
                 req = requests.put(url,
                                     headers=headers,
+                                    auth=auth,
                                     data=json.dumps(payload),
                                     files=files,
+                                    allow_redirects=redirect_allow,
                                     verify=False)
             elif method == "PATCH":
                 req = requests.patch(url,
-                                   headers=headers,
-                                   data=json.dumps(payload),
-                                   files=files,
-                                   verify=False)
-            elif method == "DELETE":
-                req = requests.delete (url,
                                      headers=headers,
+                                     auth=auth,
                                      data=json.dumps(payload),
                                      files=files,
+                                    allow_redirects=redirect_allow,
                                      verify=False)
+            elif method == "DELETE":
+                req = requests.delete (url,
+                                       headers=headers,
+                                       auth=auth,
+                                       data=json.dumps(payload),
+                                       files=files,
+                                    allow_redirects=redirect_allow,
+                                       verify=False)
         elif method == "GET":
             files = {}
-            payload = ''
-            body = self.get_payload(request_test['body'])
-            if body['method'] == 'formdata':
-                files = body['data']
-            elif body['method'] == 'raw':
-                payload = body['data']
-            req = requests.get(url, 
-                               headers=headers, 
-                               data=json.dumps(payload),
+            if auth_headers == 'outh1':
+                payload = {}
+            else:
+                body = self.get_payload(request_test['body'])
+                if body['method'] == 'formdata':
+                    payload = body['data']
+                elif body['method'] == 'raw':
+                    payload = body['data']
+            req = requests.get(url,
+                               headers=headers,
+                               auth=auth,
+                               data=payload,
                                files=files,
+                               allow_redirects=redirect_allow,
                                verify=False)
-        finish_time=datetime.now()
-        delta=(finish_time - start_time)
+        finish_time = datetime.now()
+        if debug == True:
+            print("Headers: " + json.dumps(headers))
+            print("Payload: " + json.dumps(payload))
+            print("Response: " + req.text)
+        delta = (finish_time - start_time)
         return {
+            'name' : name_test,
             'response': req.text,
             'status': str(req.status_code),
             'method' : method,
@@ -158,9 +260,12 @@ class PostmanCollection:
                 for folder in item_folder['collection']:
                     if debug is True:
                         printer.success("-----> " + folder['name'])
-                    collection_data.append(self.connection_test(folder['request']))
+                    if folder['name'] == 'OAuth1.0 Verify Signature':
+                        collection_data.append(self.connection_test(folder['request'],folder['name'], debug))
+                        sys.exit()
+
             else:      
-                collection_data.append(self.connection_test(item['request']))
+                collection_data.append(self.connection_test(item['request'],debug))
         return collection_data
 
 
